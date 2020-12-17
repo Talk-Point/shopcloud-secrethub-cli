94b621de3083129ebdd528a8fe90b29df3c0b542
Konstantin.Stoldt <konstantin.stoldt@talk-point.de>
2020-12-17 11:19:52 +0100

    cli: add read secrets

--- shopcloud-secrethub/__main__.py
+++ shopcloud-secrethub/__main__.py
@@ -1,4 +1,20 @@
+import argparse
 from . import cli
 
 if __name__ == '__main__':
-    cli.main()
+    parser = argparse.ArgumentParser(
+        description='SecretHub',
+        prog='shopcloud-secrethub'
+    )
+
+    subparsers = parser.add_subparsers(help='authentication', title='auth')
+    parser_a = subparsers.add_parser('auth', help='generate auth file')
+    parser_a.set_defaults(which='auth')
+
+    parser_b = subparsers.add_parser('read', help='read a secret')
+    parser_b.add_argument('name', type=str, help='secret name')
+    parser_b.add_argument('--output', help='Output Format', choices=['text', 'json'])
+    parser_b.set_defaults(which='read')
+
+    args = parser.parse_args()
+    cli.main(args)
--- shopcloud-secrethub/cli.py
+++ shopcloud-secrethub/cli.py
@@ -1,3 +1,79 @@
+import configparser
+import getpass
+import os
+import requests
+from typing import List
 
-def main():
-    print("Main")
+
+class ConfigFile:
+    def __init__(self, path):
+        self.path = path
+        self.config = None
+        self._load()
+
+    def _load(self):
+        try:
+            self.config = configparser.ConfigParser()
+            self.config.read(self.path)
+            self.config['default']
+        except Exception as e:
+            print("can not load token. init with auth command")
+
+    @property
+    def token(self):
+        return self.config['default']['token']
+
+    @staticmethod
+    def generate(path: str, token: str):
+        config = configparser.ConfigParser()
+        config.add_section('default')
+        config['default']['token'] = token
+
+        with open(path, 'w') as configfile:
+            config.write(configfile)
+
+
+class App:
+    def __init__(self, path, **kwargs):
+        self.config = ConfigFile(path)
+        self.endpoint = kwargs.get('endpoint', 'shopcloud-secrethub.ey.r.appspot.com')
+    
+    def read(self, secretname) -> List:
+        response = requests.get(
+            f'https://{self.endpoint}/hub/api/secrets',
+            headers={
+                'Authorization': self.config.token,
+                'User-Agent': 'secrethub-cli',
+            },
+            params={
+                'q': secretname,
+            }
+        )
+
+        return response.json().get('results', [])
+        
+
+
+def main(args):
+    path = os.path.expanduser('~/.secrethub')
+    command = args.which
+
+    if command == 'auth':
+        token = getpass.getpass('Token:')
+        ConfigFile.generate(path, token)
+    elif command == 'read':
+        app = App(path)
+        secrets = app.read(args.name)
+        if args.output is None:
+            for secret in secrets:
+                value = secret.get('value').encode('unicode_escape').decode('utf-8')
+                print(f"{secret.get('name')}={value}")
+        elif args.output == 'json':
+            for secret in secrets:
+                print(secret)
+    elif command == 'write':
+        print('write')
+    elif command == 'inject':
+        print('inject')
+    else:
+        print('No command')
